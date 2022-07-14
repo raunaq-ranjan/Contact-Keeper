1676f55d9bac130632ed466b4f276fbae9e0ee40
raunaq-ranjan <raunaq.ranjan@outlook.com>
2022-07-14 21:12:19 +0530

    JWT token and middleware for protected route

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,9 @@
 const express = require('express');
-const { check, validationResult } = require('express-validator');
-const bcrypt = require('bcryptjs');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { check, validationResult } = require('express-validator');
 
 const User = require('../models/User');
 
@@ -42,14 +44,27 @@ router.post(
 
             await user.save();
 
-            res.send('User saved');
+            const payload = {
+                user: {
+                    id: user.id
+                }
+            }
 
+            jwt.sign(
+                payload,
+                config.get('jwtSecret'),
+                {
+                    expiresIn: 360000
+                },
+                (err, token) => {
+                    if (err) throw err;
+                    res.json({ token });
+                }
+            );
         } catch (err) {
             console.error(err.message);
             res.status(500).send('Server Error');
         }
-
-        //res.send('passed');
     }
 );
 
