aeccc63fdc0dd9d2b295a0f0e0242ab2a49a09b7
raunaq-ranjan <raunaq.ranjan@outlook.com>
2022-07-15 20:44:17 +0530

    Client using react without DB connection - able to add contact

---
+++ client/src/App.js
@@ -0,0 +1,28 @@
+import { Fragment } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+import Navbar from './components/layout/Navbar';
+
+import ContactState from './context/contact/ContactState';
+import './App.css';
+
+const App = () => {
+  return (
+    <ContactState>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <div className="container">
+            <Routes>
+              <Route exact path='/' element={<Home/>} />
+              <Route exact path='/about' element={<About/>} />
+            </Routes>
+          </div>
+        </Fragment>
+      </Router>
+    </ContactState>
+  );
+}
+
+export default App;
